0759a545e30bd7a964baa88211f7b263bbbcddf1
Stinko <145304742+TheStinkern@users.noreply.github.com>
2025-05-11 13:08:46 -0400

    compilation error fix maybe

--- setup/windows.bat
+++ setup/windows.bat
@@ -17,7 +17,7 @@ haxelib git hxcpp https://github.com/HaxeFoundation/hxcpp/ --quiet
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e --quiet
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit --quiet
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90 --quiet
-haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666 --quiet
+haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git --quiet
 haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc --quiet
 haxelib install hxvlc 1.9.2 --quiet
 @echo off
