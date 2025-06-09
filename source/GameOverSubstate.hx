bad7ed810759213807671cb25092e71f6ec67463
JordanSantiagoYT <jordan.doesosu2@gmail.com>
2025-06-09 17:43:54 -0400

    band-aid fix
    should close #1084 hopefully

--- source/GameOverSubstate.hx
+++ source/GameOverSubstate.hx
@@ -230,7 +230,9 @@ class GameOverSubstate extends MusicBeatSubstate
 				overlay.offset.set(overlayConfirmOffsets.x, overlayConfirmOffsets.y);
 			}
 			FlxG.sound.music.stop();
-			FlxG.sound.play(Paths.music(endSoundName));
+			try {FlxG.sound.play(Paths.music(endSoundName));}
+			catch(e) {trace('Failed to play the end track!');}
+			
 			new FlxTimer().start(0.7, function(tmr:FlxTimer)
 			{
 				FlxG.camera.fade(FlxColor.BLACK, 2, false, function()
