2902d778ed06048756db96722419f2ec1b64d45c
Moxie <124418090+moxie-coder@users.noreply.github.com>
2025-06-19 18:02:49 -0400

    Revert "healthicon crap"
    This reverts commit 5988593f2b5d2bb741c1f27da3a3b92b5038a7c6.

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -1,23 +1,13 @@
 package;
 
-import haxe.Json;
-
 using StringTools;
 
-// metadatas for icons
-// so I don't have to deal with stupid annoying icons cutting off
-typedef IconMeta = {
-	?noAntialiasing:Bool,
-	?fps:Int,
-	?frameOrder:Array<String> // ["normal", "losing", "winning"]
-}
 class HealthIcon extends FlxSprite
 {
 	public var sprTracker:FlxSprite;
 	public var canBounce:Bool = false;
 	private var isPlayer:Bool = false;
 	private var char:String = '';
-	public var iconMeta:IconMeta;
 
 	var initialWidth:Float = 0;
 	var initialHeight:Float = 0;
@@ -47,10 +37,6 @@ class HealthIcon extends FlxSprite
 	public var iconOffsets:Array<Float> = [0, 0];
 	public function changeIcon(char:String) {
 		if(this.char != char) {
-			if (char.length < 1)
-				char = 'face';
-
-			iconMeta = getFile(char);
 			var name:String = 'icons/' + char;
 			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-' + char; //Older versions of psych engine's support
 			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-face'; //Prevents crash from missing icon
@@ -62,25 +48,13 @@ class HealthIcon extends FlxSprite
 				// throw "Don't delete the placeholder icon";
 				trace("Warning: could not find the placeholder icon, expect crashes!");
 			}
-			if (iconMeta?.frameOrder != null){
-				final framesCount:Int = Math.floor(file.width / file.height);
-				final frameWidth:Int = Math.floor(file.width / framesCount);
-				loadGraphic(file, true, frameWidth, Math.floor(file.height));
-				initialWidth = width;
-				initialHeight = height;
 
-				iconOffsets[0] = (width - 150) / framesCount;
-				iconOffsets[1] = (height - 150) / framesCount;
-			}
-			else
-			{
-				final iSize:Float = Math.round(file.width / file.height);
-				loadGraphic(file, true, Math.floor(file.width / iSize), Math.floor(file.height));
-				initialWidth = width;
-				initialHeight = height;
-				iconOffsets[0] = (width - 150) / iSize;
-				iconOffsets[1] = (height - 150) / iSize;
-			}
+			final iSize:Float = Math.round(file.width / file.height);
+			loadGraphic(file, true, Math.floor(file.width / iSize), Math.floor(file.height));
+			initialWidth = width;
+			initialHeight = height;
+			iconOffsets[0] = (width - 150) / iSize;
+			iconOffsets[1] = (height - 150) / iSize;
 
 			updateHitbox();
 
@@ -101,26 +75,6 @@ class HealthIcon extends FlxSprite
 			scale.set(mult, mult);
 			updateHitbox();
 		}
-	}
-
-	public static function getFile(name:String):IconMeta {
-		var characterPath:String = 'images/icons/$name.json';
-		var path:String = Paths.getPath(characterPath);
-		if (!Paths.exists(path, TEXT))
-		{
-			path = Paths.getPreloadPath('images/icons/bf.json'); //If a character couldn't be found, change them to BF just to prevent a crash
-		}
-
-		var rawJson = Paths.getContent(path);
-		if (rawJson == null) {
-			return null;
-		}
-
-		var json:IconMeta = cast Json.parse(rawJson);
-		if (json.noAntialiasing == null) json.noAntialiasing = false;
-		if (json.fps == null) json.fps = 24;
-		// if (json.frameOrder == null) json.frameOrder = ['normal', 'losing', 'winning'];
-		return json;
 	}
 
 	override function updateHitbox()
