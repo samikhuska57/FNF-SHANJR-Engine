9ece533aaa5cdfd5c3e813a84259ef2adcd5ebb0
Homura <egzozu.be1.bas@gmail.com>
2025-06-27 12:36:18 +0300

    Update unix.sh

--- setup/unix.sh
+++ setup/unix.sh
@@ -5,8 +5,8 @@
 echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
 sudo apt install libgl-dev libx11-dev libxi-dev libxpm-dev libxrandr-dev libncurses-dev
-haxelib git lime https://github.com/th2l-devs/lime 3cf643f416e05eb700e80e4cca6868329793e370 --quiet
-haxelib git openfl https://github.com/th2l-devs/openfl 6160e49a7da61fd8b3f83f1b70f76f07567ad4c7 --quiet
+haxelib git lime https://github.com/th2l-devs/lime --quiet
+haxelib git openfl https://github.com/th2l-devs/openfl --quiet
 haxelib git flixel https://github.com/JS-Engine-things/flixel-JS-Engine --quiet
 haxelib install flixel-addons 3.2.3 --quiet
 haxelib install flixel-tools 1.5.1 --quiet
@@ -21,5 +21,5 @@ haxelib git linc_luajit https://github.com/th2l-devs/linc_luajit --quiet
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90 --quiet
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git --quiet
 haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc --quiet
-haxelib install hxvlc 1.9.2 --quiet
+haxelib git hxvlc https://github.com/th2l-devs/hxvlc --quiet
 echo Finished!
