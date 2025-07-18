a8e51a42ed82b8273c1c02b003c7976a1f8f4218
Andre Nicholas Jr <151701402+Andrhevn23Rbx@users.noreply.github.com>
2025-07-18 12:53:18 -0500

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -26,7 +26,7 @@ class MainMenuState extends MusicBeatState
 {
 	public static final gitCommit:String = HaxeCommit.getGitCommitHash();
 
-	public static var psychEngineJSVersionNumber:String = '1.48.1'; //This is also used for Discord RPC
+	public static var psychEngineJSVersionNumber:String = 'SHANJR 0.0.1'; //This is also used for Discord RPC
 	public static var psychEngineJSVersion:String = psychEngineJSVersionNumber #if commit + ' (Commit $gitCommit)' #end; //This is also used for Discord RPC
 	public static var psychEngineVersion:String = '0.6.3'; //This is also used for Discord RPC
 	public static var curSelected:Int = 0;
